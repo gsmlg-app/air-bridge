36095ed6d02251a741f920356da3957f105bf63f
Jonathan Gao <gsmlg.com@gmail.com>
2026-04-15 21:59:08 +0800

    feat: add AVRoutePickerView NSViewRepresentable wrapper

---
+++ Sources/AirBridge/MenuBar/RoutePickerWrapper.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import AVKit
+
+struct RoutePickerWrapper: NSViewRepresentable {
+    func makeNSView(context: Context) -> AVRoutePickerView {
+        let picker = AVRoutePickerView()
+        picker.isRoutePickerButtonBordered = false
+        return picker
+    }
+
+    func updateNSView(_ nsView: AVRoutePickerView, context: Context) {}
+}
